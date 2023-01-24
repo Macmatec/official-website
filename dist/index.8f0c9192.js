b549e5f4488904d935e90e3d5b095a9045c3eed6
teddy-thie <ted.thie@outlook.com>
2023-01-24 11:12:19 +0300

    This is a commit

---
+++ dist/index.8f0c9192.js
@@ -0,0 +1,39 @@
+let tl = gsap.timeline({
+    defaults: {
+        ease: "power4.inOut",
+        duration: 2
+    }
+});
+let dots = CSSRulePlugin.getRule(".card:before");
+tl.to(".hero-tag", {
+    "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+    opacity: 1,
+    y: 0,
+    duration: 2.2
+}).to(".hero-headline", {
+    "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+    opacity: 1,
+    y: 0,
+    duration: 2.2
+}, "-=2").to(".hero-mouse", {
+    "clip-path": "polygon(0% 100%, 100% 100%, 100% 0%, 0% 0%)",
+    opacity: 1,
+    y: 0,
+    duration: 1.5
+}, "-=1").from(".card", {
+    scaleY: 0,
+    stagger: .2
+}, "-=1").from(dots, {
+    stagger: 1,
+    opacity: 0,
+    transform: "translateY(100px)"
+}, "-=1").to(".card-title, .card-desc", {
+    stagger: .1,
+    duration: 1.2,
+    opacity: 1,
+    y: 0
+}, "-=1").to("footer", {
+    opacity: 1
+}, "-=2");
+
+//# sourceMappingURL=index.8f0c9192.js.map
